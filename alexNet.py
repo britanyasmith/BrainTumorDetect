b00e50b165228d2fc3b0d15d7eb728ed5921d293
Britanya Wright-Smith <britanya.rsmith@gmail.com>
2023-02-21 06:45:08 -0500

    new file:   __pycache__/alexNet.cpython-310.pyc 	new file:   __pycache__/model.cpython-310.pyc 	deleted:    alexNet.py 	modified:   main.py 	new file:   model.py

--- alexNet.py
+++
@@ -1,2 +0,0 @@
-import tensorflow as tf 
-from d2l import tensorflow as d2l 
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 #This is the python script where the entire operation of Brain tumor Detection is ran from 
+from  model import Model
+ 
 
 
+Model().layer_summary((1,224,224,1))
