2758a4406205c535f7fad63292085621b86b7c97
Britanya Wright-Smith <britanya.rsmith@gmail.com>
2023-02-22 14:32:38 -0500

    new file:   data/Testing/glioma_tumor/image(1).jpg

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #This is the python script where the entire operation of Brain tumor Detection is ran from 
-from  model import Model
+#from  model import Model
+import pandas as pd 
+import requests
+from PIL import Image
+from io import BytesIO
+import zipfile 
+import os 
  
+#Obtain the MRI Images and visualize them
+path_train = '/data/Training'
+path_test = '/data/Testing'
 
 
-Model().layer_summary((1,224,224,1))
+
